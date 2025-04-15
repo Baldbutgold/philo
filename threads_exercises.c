c898c4922b3089f9cef5c056e8795a37fb535019
abdou <abdou@glmail.coom>
2025-04-15 19:37:34 +0100

    mutex

--- threads_exercises.c
+++ threads_exercises.c
@@ -1,28 +1,54 @@
 #include "philo.h"
+#include <pthread.h>
+#define n 2
+
+int	count;
+pthread_mutex_t mutex;
 
 void	*write_code(void *coder)
 {
-	printf("Is reading theory\n");
-	sleep(2);
-	printf("Code is Ready\n");
+	int	i = 0;
+	while (i < 100000000)
+	{
+		pthread_mutex_lock(&mutex);
+		count++;
+		pthread_mutex_unlock(&mutex);
+		i++;
+	}
 	return (NULL);
 }
 
-int main(int ac, char **av)
+int	main()
 {
-	pthread_t	abdou;
-	pthread_t	mouad;
-	pthread_t	ahmed;
-	pthread_t	ayman;
+	pthread_t a[n];
+	int	i;
 
-
-	pthread_create(&abdou, NULL, write_code, NULL);
-	pthread_create(&mouad, NULL, write_code, NULL);
-	pthread_create(&ahmed, NULL, write_code, NULL);
-	pthread_create(&ayman, NULL, write_code, NULL);
-
-	pthread_join(abdou, NULL);
-	// pthread_join(mouad, NULL);
-	// pthread_join(ahmed, NULL);
-	// pthread_join(ayman, NULL);
+	pthread_mutex_init(&mutex, NULL);
+	i = 0;
+	while (i < n)
+		if (pthread_create(&a[i++], NULL, write_code, NULL))
+			return (printf("crashed at creation"), 1);
+	i = 0;
+	while (i < n)
+		if (pthread_join(a[i++], NULL))
+			return (printf("crashed at join"), 1);
+	pthread_mutex_destroy(&mutex);
+	i = 0;
+	printf("%d\n", count);
 }
+
+/*int main(int ac, char **av)*/
+/*{*/
+/*	pthread_t	a;*/
+/*	pthread_t	b;*/
+/**/
+/*	if (pthread_create(&a, NULL, write_code, NULL))*/
+/*		return (1);*/
+/*	if (pthread_create(&b, NULL, write_code, NULL))*/
+/*		return (1);*/
+/*	if (pthread_join(a, NULL))*/
+/*		return (1);*/
+/*	if (pthread_join(b, NULL))*/
+/*		return (1);*/
+/*	printf("%d\n", count);*/
+/*}*/
